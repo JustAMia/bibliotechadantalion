031ca8ee5ef3a3c80af8555eed38b9d9c97a7123
Iko <iko.jdr@gmail.com>
2025-11-25 17:12:44 +0100

    site: CONFIG.URL, in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { CONFIG } from "@config";
+
 
 import { remarkExtendImage, remarkExtendBlockquote } from "./src/js/plugins/remark-plugins";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.mywebsite.com',
+  site: CONFIG.URL,
   base: '/',
 
   devToolbar: {
